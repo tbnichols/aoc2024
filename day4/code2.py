fa18df6e7c5110e3e803883aefbaad0042634ea0
Tabitha Nichols <tabitha.b.nichols@gmail.com>
2024-12-04 12:41:07 -0500

    what if worse

--- day4/code2.py
+++ day4/code2.py
@@ -8,9 +8,7 @@ for x in f:
 for i in range(len(search)):
 	for j in range(len(search[0])):
 		if search[i][j] =='A':
-			if 0<=(i-1) and (i+1)<len(search) and 0<=(j-1) and (j+1)<len(search[0]):
-				mOcc = list(filter(lambda x: search[i+x[1]][j+x[0]] == 'M', directions))
-				sOcc = list(filter(lambda x: search[i+x[1]][j+x[0]] == 'S', directions))
-				if len(mOcc) == len(sOcc) == 2 and search[i-1][j-1] != search[i+1][j+1]:
+			if 0<=(i-1) and (i+1)<len(search) and 0<=(j-1) and (j+1)<len(search[0]) and search[i-1][j-1] != search[i+1][j+1]:
+				if reduce(lambda a,b: a and b, map(lambda r: len(r) ==2, map(lambda q: list(filter(lambda x: search[i+x[1]][j+x[0]] == q, directions)), ["M", "S"]))):
 					total+=1
 print(total)	
