967bd8e90e5d39c5da0d9b13b5f115f4c011982e
Tabitha Nichols <tabitha.b.nichols@gmail.com>
2024-12-17 20:17:41 -0500

    some comments in part 2

--- day17/code2.py
+++ day17/code2.py
@@ -10,6 +10,7 @@ ins = [int(i) for i in f.readline().strip().split(':')[1].strip().split(',')]
 print(ins[::-1])
 out=[]
 numpots = ["0000000000"]
+ #input is 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
 for i in ins[::-1]:
     tempnums = []
     for num in numpots:
@@ -17,10 +18,12 @@ for i in ins[::-1]:
             potnum = num+bpot
             # print(num)
             # print(bpot)
+            # c is bitshifted x out of A where x is B^5
             C = int(potnum[len(potnum)-(int(bpot,2)^5)-3:len(potnum)-(int(bpot,2)^5)], 2)
             if int(bpot,2)^5^6^C == i:
                 tempnums.append(potnum)
                 calcans = int(bpot,2)^5^6^C
+                # output is B^C^5^6
                 print(f"goal: {i}, C: {C}, B: {bpot}, ans: {calcans}")
     numpots = tempnums
     print(numpots)
@@ -28,4 +31,5 @@ for i in ins[::-1]:
     input()
 print(numpots)
 print([int(num,2) for num in numpots])
+# generating all possibles, taking the min for it
 print(min([int(num,2) for num in numpots]))
